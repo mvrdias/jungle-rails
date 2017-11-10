6e36fe3b17b0f17458a16eade788a24d2cf586b8
Marcus Dias <mvrdias@uol.com.br>
2017-11-10 04:34:03 +0000

    Including order-details

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,5 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_UN'], password: ENV['ADMIN_PW']
 
   def index
     @categories = Category.order(id: :desc).all
